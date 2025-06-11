02fbfafb542d1edafeb864e6e0eb2f2aa75be0a3
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-06-11 14:14:17 +0000

    usando Cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import nslookupRoutes from './routes/nslookup';
 import whoisRoute from './routes/whois';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/nslookup', nslookupRoutes);
 app.use('/whois', whoisRoute);
