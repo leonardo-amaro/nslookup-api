b1d7d75b6cdd02c63161b8992ae7f5f22605a878
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-06-22 15:32:56 +0000

    adicionando consulta PTR

--- src/routes/nslookup.ts
+++ src/routes/nslookup.ts
@@ -5,17 +5,19 @@ const router = express.Router();
 
 router.get('/:domain', async (req, res) => {
   const { domain } = req.params;
+  const [ip] = await dns.resolve4(domain);
 
   try {
-    const [a, aaaa, cname, mx, ns] = await Promise.all([
+    const [a, aaaa, cname, mx, ns, ptr] = await Promise.all([
       dns.resolve4(domain).catch(() => []),
       dns.resolve6(domain).catch(() => []),
       dns.resolveCname(domain).catch(() => []),
       dns.resolveMx(domain).catch(() => []),
       dns.resolveNs(domain).catch(() => []),
+      dns.reverse(ip),
     ]);
 
-    res.json({ domain, a, aaaa, cname, mx, ns });
+    res.json({ domain, a, aaaa, cname, mx, ns, ptr });
   } catch (err: any) {
     res.status(500).json({ error: 'Erro ao consultar DNS', detail: err.message });
   }
